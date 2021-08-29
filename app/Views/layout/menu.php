ee111f10d3f9338738b66a121e72a01d351a434a
doniambarita <doniambarita123@gmail.com>
2021-08-29 14:39:38 +0700

    add menu layout

---
+++ app/Views/layout/menu.php
@@ -0,0 +1,12 @@
+<?= $this->extend('layout/main'); ?>
+
+<?= $this->section('menu'); ?>
+<li class="has-submenu">
+    <a href="<?= site_url('layout/index'); ?>"><i class="mdi mdi-airplay"></i>Beranda</a>
+</li>
+
+<li class="has-submenu">
+    <a href="<?= site_url('siswa/index'); ?>">Siswa</a>
+</li>
+
+<?= $this->endSection(); ?>
